46baeff91ab8700aa70b29b930006752ab4976d8
LeonXi <cdxiyang@gmail.com>
2018-02-02 17:08:44 +0800

    ok

--- src/test/java/com/kuangkee/search/TestArticleSearchService.java
+++ src/test/java/com/kuangkee/search/TestArticleSearchService.java
@@ -10,12 +10,10 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.kuangkee.service.solr.IArticleSearchService;
 
-
-
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration({ "classpath:spring/applicationContext-*.xml" })
 public class TestArticleSearchService {
-	Logger log = LoggerFactory.getLogger(TestArticleSearchService.class) ;
+	private static final Logger log = LoggerFactory.getLogger(TestArticleSearchService.class) ;
 	
 	@Autowired
 	IArticleSearchService searchService ;
@@ -30,7 +28,7 @@ public class TestArticleSearchService {
 		String qryStr = "手机" ;
 		int page = 1 ;
 		int rows = 20 ;
-		
+		log.info("----------> info");
 //		SearchResult<Article> result = searchService.search(qryStr, page, rows) ;
 //		log.info("[查询结果]-->"+result.getResult());
 //		System.out.println(result.getResult());
